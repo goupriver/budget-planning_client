288fed28dc06a22144f4001f20cad26d0414656d
Alexander Mironov <goupriver@gmail.com>
2022-07-13 02:07:05 +0700

    fix(components): added props (id, name)

--- src/components/forms/TextField/TextField.jsx
+++ src/components/forms/TextField/TextField.jsx
@@ -2,11 +2,13 @@ import React from "react";
 
 import styles from "./TextField.module.css";
 
-export const TextField = ({ onChange, value, type = "text" }) => {
+export const TextField = ({ onChange, value, name, id, type = "text" }) => {
   return (
     <input
+      name={name}
+      id={id}
       type={type}
-      className={styles.module}
+      className={styles.input}
       onChange={onChange}
       value={value}
     />
