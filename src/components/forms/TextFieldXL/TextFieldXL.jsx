388eb5eb0b6483696f6246bb79747b8eaaac4cb2
Alexander Mironov <goupriver@gmail.com>
2022-07-14 22:58:32 +0700

    feat(components): added new props placeholder

--- src/components/forms/TextFieldXL/TextFieldXL.jsx
+++ src/components/forms/TextFieldXL/TextFieldXL.jsx
@@ -2,12 +2,13 @@ import React from "react";
 
 import styles from "./TextFieldXL.module.css";
 
-export const TextFieldXL = ({ onChange, value, name, id, type = "text" }) => {
+export const TextFieldXL = ({ onChange, value, name, id, type = "text", placeholder }) => {
   return (
     <input
       name={name}
       id={id}
       type={type}
+      placeholder={placeholder}
       className={styles.input}
       onChange={onChange}
       value={value}
