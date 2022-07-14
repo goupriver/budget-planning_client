ac6ab115be79da653d9869327ee4a631a62157a2
Alexander Mironov <goupriver@gmail.com>
2022-07-14 13:53:50 +0700

    feat(components): add new icon

--- src/components/media/Icon/getIconByCategoryName.js
+++ src/components/media/Icon/getIconByCategoryName.js
@@ -1,17 +1,19 @@
 export function getIconByCategoryName(category) {
   const list = {
-    'Bills': 'description', 
-    'Food': 'local_pizza',
-    'Clothes': 'checkroom',
-    'Transport': 'drive_eta',
-    'Fun': 'videogame_asset',
-    'Other': 'file_present',
-    'feed': 'feed',
-    'stats': 'trending_up',
-    'settings': 'settings',
-    'add': 'add',
-    'remove': 'remove'
-  } 
+    Bills: "description",
+    Food: "local_pizza",
+    Clothes: "checkroom",
+    Transport: "drive_eta",
+    Fun: "videogame_asset",
+    Other: "file_present",
+    feed: "feed",
+    stats: "trending_up",
+    settings: "settings",
+    add: "add",
+    remove: "remove",
+    chevronRight: "chevron_right",
+    calendar: "calendar_month",
+  };
 
-  return list[category]
+  return list[category];
 }
