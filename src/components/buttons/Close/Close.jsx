4ac321ae68851a708af75f4ee9155aded54f9f97
Alexander Mironov <goupriver@gmail.com>
2022-07-14 22:51:16 +0700

    style(components): removed logger

--- src/components/buttons/Close/Close.jsx
+++ src/components/buttons/Close/Close.jsx
@@ -1,7 +1,6 @@
 import styles from "./Close.module.css";
 
 export const Close = ({ onClick, variant }) => {
-  console.log(styles);
   return (
     <button className={styles.close + " " + styles[variant]} onClick={onClick}>
       <span className={"material-icons " + styles["mi" + variant]}>close</span>
