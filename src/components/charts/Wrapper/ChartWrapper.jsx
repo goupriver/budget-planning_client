714f2deeeab78278209f0b90b9122024f9fc3e05
Alexander Mironov <goupriver@gmail.com>
2022-07-14 22:54:14 +0700

    feat(components): added new props

--- src/components/charts/Wrapper/ChartWrapper.jsx
+++ src/components/charts/Wrapper/ChartWrapper.jsx
@@ -1,5 +1,11 @@
 import styles from "./ChartWrapper.module.css";
 
-export const ChartWrapper = ({ children }) => {
-  return <div className={styles.wrapperChart}>{children}</div>;
+export const ChartWrapper = ({ children, swiper = false, mb = false }) => {
+  const isMarginSwiper = swiper ? styles.swiper : "";
+  const margin = mb ? styles.mb : "";
+  return (
+    <div className={styles.wrapperChart + " " + isMarginSwiper + " " + margin}>
+      {children}
+    </div>
+  );
 };
